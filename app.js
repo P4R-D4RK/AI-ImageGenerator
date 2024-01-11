2f5655c93c71c0137a1d235d4f0a1401fd8a24fd
P4R-D4RK <luis.282001@gmail.com>
2024-01-11 10:40:44 -0600

    final commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const API_KEY = 'd784ede1-781f-4611-809c-a1ec0c76a6c9';
+const submitIcon = document.querySelector('#submit-icon');
+const inputElement = document.querySelector('input');
+const imageSection = document.querySelector('.images-section');
+
+const getImages = async () => {
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt: inputElement.value,
+      size: "1024x1024",
+    }),
+  };
+  try {
+    const response = await fetch('https://api.openai.com/v1/images/generations', options);
+    const data = await response.json();
+    // console.log(data);
+    if(data) {
+        const ImageContainer = document.createElement('div');
+        ImageContainer.classList.add('image-container');
+        const imageElement = document.createElement('img');
+        imageElement.setAttribute('src', data.data[0].url)
+        ImageContainer.append(imageElement);
+        imageSection.append(ImageContainer);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+submitIcon.addEventListener('click', getImages);
